fd9e0ee674d31a3f537cb1a18a7325962c77e385
Ekain Cao <>
2020-03-23 14:26:00 +0800

    get some index point

---
+++ services/getdata/getBigSmallIndexPoint.js
@@ -0,0 +1,31 @@
+'use strict';
+
+exports.GetBigSmallIndexPoint = () => {
+    return {
+        GameId:"1173677",
+        BigSmallIndexPoint: [
+            {
+                Cid: '3000181',
+                Name: 'Bet365',
+                Logs: [
+                    {
+                        Data: [5.5, 1.15, 5.5],//【大球，小球，总比分】
+                        Date: "1417776519000"
+                    },
+                    {
+                        Data: [4.65, 1.18, 5.5],
+                        Date: "1417776342000"
+                    },
+                    {
+                        Data: [4.15, 1.22, 5.5],
+                        Date: "1417776284000"
+                    },
+                    {
+                        Data: [2.85, 1.4, 5.5],
+                        Date: "1417776105000"
+                    },
+                ]
+            }
+        ]
+    };
+}
---
+++ services/getdata/getEuropeanIndexPointRun.js
@@ -0,0 +1,30 @@
+exports.GetEuropeanIndexPointRun = () => {
+    return {
+        GameId: "1173677",
+        EuropeanIndexPointRun: [
+            {
+                Cid: '40000',
+                Name: '',
+                IsRun:1,
+                Logs: [
+                    {
+                        Data:[2.53,4.05,2.1],//【主胜，和局，客胜】
+                        Date:"1417775905000"
+                    },
+                    {
+                        Data:[2.6,3.9,2.09],
+                        Date:"1417775738000"
+                    },
+                    {
+                        Data:[2.71,3.7,2.09],
+                        Date:"1417775649000"
+                    },
+                    {
+                        Data:[2.78,3.55,2.08],
+                        Date:"1417775478000"
+                    },
+                ]
+            }
+        ]
+    };
+}
